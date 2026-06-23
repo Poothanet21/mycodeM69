5592d9dea8d7603dbad9c085ff6846b63b75634e
Poothanet21 <467112@wsk.ac.th>
2026-06-23 01:37:19 +0000

    pro03.py

---
+++ pro03.py
@@ -0,0 +1,10 @@
+king=float(input("ความกว้าง:")) #รับค่าจากความกว้าง
+new=float(input("ความยาว:")) #รับจากความยาว
+
+area=king*new #คำนวนแล้วเอามาบวกกัน
+perimeter=2*(king+new) #เส้นรอบรูป กว้างxยาว
+
+print("king:",area) #แสดงพื้นที
+print("new:",perimeter) #แสดงเส้นรอบรูป
+
+print("author by Rachata")
